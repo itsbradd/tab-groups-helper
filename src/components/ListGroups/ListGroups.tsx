a058f2b13b09abc0afbee19c3e79feebd923c325
Brad <sonngoc.me@gmail.com>
2022-07-19 18:26:58 +0700

    Add add button in list groups page

--- src/components/ListGroups/ListGroups.tsx
+++ src/components/ListGroups/ListGroups.tsx
@@ -1,9 +1,15 @@
 import { Group } from '../Group';
+import {IconButton} from "@mui/material";
+import AddIcon from '@mui/icons-material/Add';
+import './ListGroups.scss';
 
 export const ListGroups = () => {
 	return (
 		<div className='list-groups'>
 			<Group />
+			<IconButton className='button-add' color='primary'>
+				<AddIcon />
+			</IconButton>
 		</div>
 	);
 };
